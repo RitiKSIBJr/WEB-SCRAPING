8554f8ac327d7e5b2a4e21c4ffb5048333e92854
RitiKSiBJr <letter.ritik060@gmail.com>
2022-11-24 11:15:47 +0545

    beautiful soup, lxml, request

---
+++ Request Libray/scrape.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from lxml import html
+
+#request
+base_url = "https://github.com/topics/python"
+response = requests.get(base_url)
+
+# Beautifulsoup4
+soup = BeautifulSoup(response.text, 'html.parser')
+
+#without css selectors
+repo = soup.findAll("h3", attrs={"class":"f3 color-fg-muted text-normal lh-condensed"})
+
+# with css selectors
+# repos = soup.select("h3.f3 color-fg-muted text-normal lh-condensed")
+
+# lxml
+# tree = html.fromstring(response.text)
+# title = tree.xpath('//h2[@class="blog-card__content-title"]/text()')
+python = []
+
+link = soup.findAll('a', attrs={"class":"text-bold wb-break-word"}, href=True)
+for x in link:
+    head = x.text.strip()
+    url = base_url+x['href']
+    info = {'title':head, 'url':url}
+    python.append(info)
+
+print(python)
