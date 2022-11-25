a9e3d55eb725cc77d0f37a4c4442f8a6d99f7cbf
RitiKSiBJr <letter.ritik060@gmail.com>
2022-11-25 14:28:09 +0545

    m

--- Request Libray/scrape.py
+++ Request Libray/scrape.py
@@ -24,21 +24,18 @@ repo = soup.findAll("h3", attrs={"class":"f3 color-fg-muted text-normal lh-conde
 # tree = html.fromstring(response.text)
 # title = tree.xpath('//h2[@class="blog-card__content-title"]/text()')
 python = []
-info = defaultdict()
+
 
 link = soup.findAll('a', attrs={"class":"text-bold wb-break-word"}, href=True)
 for x in link:
+    info = defaultdict()
     head = x.text.strip()
     url = base_url+x['href']
     info[head] = url
+    python.append(dict(info))
 
-    with open("scrape.json", "a") as file:
-            json.dump(info, file)
-            file.write('\n')
-            info = defaultdict()
+with open('scrape.json', 'w') as file:
+    json.dump(python, file, indent=2)
 
-
-with open("scrape.json", "r") as read_file:
-    for line in read_file:
-        data = json.loads(line)
-        print(data)
+with open('scrape.json','r') as f:
+    data = json.load(f)
